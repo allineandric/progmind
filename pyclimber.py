5bf394d50503cb3f903577e54317991d195be558
Alline Andric <andric.alline@gmail.com>
2020-10-24 11:52:35 -0300

    initial commit

---
+++ pyclimber.py
@@ -0,0 +1,54 @@
+"""This module is the main entry for the Py-Climber game"""
+
+import src.game_functions as gf
+from src.image_resources import ImageResources
+from src.settings import Settings
+from src.tilemap import Tilemap
+import random
+import pygame
+
+def run_game():
+    """Main entry point for Py-Climber"""
+
+    # Startup pygame object
+    pygame.init()
+
+    random.seed()
+
+    # Load our settings object and image resources, disk I/O that can be done in advance
+    settings = Settings()
+    image_res = ImageResources(settings)
+    # Add to the cache so it's accessible where needed
+    settings.image_res = image_res
+
+    # Create the main screen to render to based on settings
+    screen = pygame.display.set_mode((settings.screen_width, settings.screen_height))
+    pygame.display.set_caption(settings.caption)
+    
+    # Create a 2D tilemap - this takes a list of indices and an image list to produce a tiled surface
+    tile_map = Tilemap(settings, screen, settings.map_indicies, image_res.tile_images, 
+        image_res.block_image, image_res.blob_exit_images, image_res.player_sprite_images, image_res.enemy_blob_images)
+
+    # Overwrite default indices with generated map 
+    tile_map.generate_basic_map(settings.map_number_floors , settings.map_number_subfloors)
+
+    # Reset the game
+    gf.reset_game(tile_map)
+
+    # Use pygame's simple loop management for a fixed 30 FPS
+    clock = pygame.time.Clock()
+    while True:
+        # Should make sure each frame spends at least 1/30 seconds in this loop
+        # downside is wasted sleep on fast hardware and slow hardware will lag
+        # but slow hardware will always lag and implementing a time-delta based
+        # loop for this simple game is IMHO overkill.
+        clock.tick(30)
+
+        # Process system events (key-presses, joystick, etc)
+        gf.check_events(settings, screen, tile_map)
+
+        # Update the game (this will update all sub-object and render them to the screen)
+        gf.update_screen(settings, screen, tile_map)
+    
+# Invokes the function above when the script is run
+run_game()
---
+++ src/game_functions.py
@@ -0,0 +1,144 @@
+"""This module implements standard game functions for Py-Climber, such as processing keypresses"""
+
+import sys
+import random
+from src.blob_enemy import Blob
+import pygame
+import pygame.freetype
+
+def check_events(settings, screen, tile_map):
+    """Watch for keyboard and mouse events"""
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+        elif event.type == pygame.KEYDOWN:
+            check_keydown_events(settings, event, screen, tile_map)
+        elif event.type == pygame.KEYUP:
+        	check_keyup_events(settings, event, screen, tile_map)
+
+def reset_game(tile_map):
+    tile_map.reset()
+
+def check_keydown_events(settings, event, screen, tile_map):
+    """Respond to key down events"""
+    player = tile_map.player
+    if event.key == pygame.K_ESCAPE:
+        sys.exit()
+
+    if event.key == pygame.K_a:
+        generate_new_random_blob(settings, screen, settings.image_res.enemy_blob_images, tile_map)
+        
+    if event.key == pygame.K_r:
+        reset_game(tile_map)
+    
+    if event.key == pygame.K_LEFT:
+        if not player.idle_top:
+            if player.dx == 0.0:
+                player.dx = -1 * settings.player_dx
+                player.facing_left = True
+    
+    if event.key == pygame.K_RIGHT:
+        if not player.idle_top:
+            if player.dx == 0.0:
+                player.dx = settings.player_dx
+                player.facing_left = False
+        
+    if event.key == pygame.K_F9:
+        if settings.fullscreen == True:
+            settings.fullscreen = False
+            pygame.display.set_mode((800, 600))
+        else:
+            settings.fullscreen = True
+            pygame.display.set_mode((800, 600), pygame.FULLSCREEN)
+
+def check_keyup_events(settings, event, screen, tile_map):
+    player = tile_map.player
+    if event.key == pygame.K_SPACE:
+        if not player.idle_top:
+            if player.falling == False:
+                player.dy = settings.player_jump_velocity
+                player.falling = True
+            elif player.air_jumps < player.max_air_jumps:
+                player.dy = settings.player_air_jump_velocity
+                player.air_jumps += 1
+
+    if event.key == pygame.K_LEFT:
+        if not player.idle_top:
+            if player.dx != 0.0:
+                player.dx = 0.0
+        
+    if event.key == pygame.K_RIGHT:
+        if not player.idle_top:
+            if player.dx != 0.0:
+                player.dx = 0.0
+
+def generate_new_random_blob(settings, screen, images, tile_map):
+    """Generate a new blob enemy and add it to the list"""
+    # How this should work:  First pick a floor, this is the middle_row of the triad created
+    # when generating the map, e.g. not the floor and not a level where blocks can appear
+    floor_number = random.randint(0, settings.map_number_floors - 2)
+
+    # Secondly pick a side, left or right (this will affect placement and initial velocity, etc)
+    facing_left = random.choice([True, False])
+
+    # Calculate initial position / velocity / facing flags
+    enemy = Blob(settings, screen, images)
+    enemy.rect.bottom = settings.tile_height * ( 2 + (3 * floor_number))
+    enemy.rect.left = 3 * settings.tile_width + tile_map.x_offset
+    enemy.dx = settings.enemy_blob_dx
+
+    if facing_left:
+        enemy.rect.left += 10 * settings.tile_width
+        enemy.dx *= -1.0
+        enemy.facing_left = True
+        enemy.set_current_animation(settings.anim_name_walk_left)
+    else:
+        enemy.facing_left = False
+        enemy.set_current_animation(settings.anim_name_walk_right)
+
+    # Add it to the list
+    tile_map.enemies.add(enemy)
+    
+def blit_help_text(settings, screen):
+    """Draws the text explaining what keys do what"""
+    color_white = (255, 255, 255)
+    y = screen.get_rect().bottom - 48
+    font = settings.font
+    font.render_to(screen, (10,y), "ESC to exit", settings.font_color)
+    y -= 20
+    font.render_to(screen, (10,y), "F9 to toggle fullscreen", settings.font_color)
+    y -= 20
+    font.render_to(screen, (10,y), "'a' to add a new enemy", settings.font_color)
+    y -= 20
+    font.render_to(screen, (10,y), "'r' to reset", settings.font_color)
+    y -= 20
+    font.render_to(screen, (15,y), "...can jump once in air", settings.font_color)
+    y -= 20
+    font.render_to(screen, (10,y), "SPACE to jump", settings.font_color)
+    y -= 20
+    font.render_to(screen, (10,y), "LEFT/RIGHT arrows to walk", settings.font_color)
+    
+def update_game_objects(settings, tile_map):
+    tile_map.update()
+
+def draw_game_objects(settings, screen, tile_map):
+    # Draw the map - pass True to render a grid overlay on the tiles
+    tile_map.draw()
+
+    # Draw help text
+    blit_help_text(settings, screen)
+
+def update_screen(settings, screen, tile_map):
+    """Update images and flip screen"""
+    # Redraw screen each pass
+    screen.fill(settings.bg_color)
+
+    # UPDATES...
+    update_game_objects(settings, tile_map)
+
+    # DRAWS...
+    draw_game_objects(settings, screen, tile_map)
+
+    # FLIP....
+    pygame.display.flip()
